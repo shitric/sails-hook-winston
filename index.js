25027a20bde8ef6d0a119e16103901e08d425694
andruxa <shitric2@gmail.com>
2015-06-17 18:19:40 +0400

    Add mail and transports

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var winston = require('winston');
+var Mail = require('winston-mail').Mail;
 var path = require('path');
 var mkdirp = require('mkdirp');
 var captain = require(path.resolve('node_modules/sails/node_modules/captains-log'));
@@ -28,8 +29,30 @@ module.exports = function(sails) {
         }
       };
 
+      var transports = [ new winston.transports.Console(consoleOptions)]
+
+      sails.config.log.transports.forEach(function(transport){
+        transports.push(transport);
+      })
+
+
+
       // Console Transport
-      logger = new winston.Logger({transports: [ new winston.transports.Console(consoleOptions)]});
+      logger = new winston.Logger({transports: transports});
+
+      if(sails.config.log.mail){
+        logger.add(Mail, {
+          host: sails.config.log.mail.host,
+          port: sails.config.log.mail.port,
+          ssl: sails.config.log.mail.ssl || true,
+          username: sails.config.log.mail.username, // GMAIL ACCOUNT HERE
+          password: sails.config.log.mail.password,
+          subject: sails.config.log.mail.subject || '', // EX: 'Hi i'm a bot!'
+          from: sails.config.log.mail.from, // EX: 'I'M A BOT <bot@test.com>'
+          to: sails.config.log.mail.to, // EX: 'BOT1 <bot1@test.com>, BOT2 <bot2@test.com>, BOT3 <bot3@test.com>'
+          level: sails.config.log.mail.level || 'error'
+        });
+      }
 
       // DailyRotateFile Transport
       if (sails.config.log.dailyRotate) {
@@ -42,10 +65,12 @@ module.exports = function(sails) {
         logger.add(require('winston-mongodb').MongoDB, sails.config.log.mongoDB);
       }
 
+
+
       sails.config.log.custom = logger;
 
       log = captain(sails.config.log);
-      log.ship = buildShipFn(sails.version ? ('v' + sails.version) :'', log.info );
+      log.ship = buildShipFn(sails.version ?('v' + sails.version) :'', log.info );
       sails.log = log;
       return done();
     }
